5f858091eb85d0d095e2e14b5cefdb552bdda167
Nguyen Duy Tu <nguyenduytu75it>
2020-04-24 10:21:49 +0700

    aaa

--- assets/js/data.config.js
+++ assets/js/data.config.js
@@ -1,5 +1,5 @@
 const urlData = [
     "https://raw.githubusercontent.com/nguyenduytu75it/nguyenduytu75it.github.io/master/data/data01.csv",
-    "https://raw.githubusercontent.com/nguyenduytu75it/nguyenduytu75it.github.io/master/data/data02.csv",
-    "https://raw.githubusercontent.com/nguyenduytu75it/nguyenduytu75it.github.io/master/data/data03.csv"
+    // "https://raw.githubusercontent.com/nguyenduytu75it/nguyenduytu75it.github.io/master/data/data02.csv",
+    // "https://raw.githubusercontent.com/nguyenduytu75it/nguyenduytu75it.github.io/master/data/data03.csv"
 ]
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
-    $("#initial-btn").click(function () {
         initialData()
-    })
 });
 
 function initialData() {
@@ -14,10 +12,11 @@ function initialData() {
             divContentFile = "<div class='col-md-12 content-file' id='file-" + fileName + "'>"
             divFileName = "<div class='col-md-12 file-name'><h3>" + fileName + "</h3></div>"
             lstCategories = getCategories(data)
+            divCategory = ""
 
-            for (let y = 0; y <= lstCategories.length; y++) {
-                divCategory = "<div class='col-md-12 category'><div class='category-title' id='category-title-1'>" + lstCategories[y] + "</div></div>"
-            }
+            lstCategories.forEach(category => {
+                divCategory += "<div class='col-md-12 category'><div class='category-title' id='category-title-1'>" + category + "</div></div>"
+            });
 
             $(".content-main").append(divContentFile + divFileName + divCategory + "</div>");
         })
@@ -31,19 +30,11 @@ function initialData() {
 function getCategories(data) {
     let = arrCategories = []
     var allTextLines = data.split(/\r\n|\n/);
-    for (let i = 0; i <= allTextLines.length; i++) {
+    for (let i = 1; i <= allTextLines.length - 1; i++) {
         lineSplit = allTextLines[i].split(',')
-        console.log(lineSplit[2])
-        arrCategories.push(lineSplit[2])
+        arrCategories.push(lineSplit[2].replace('"',""))
     }
     return arrCategories
-}
-
-function processData(data) {
-    var allTextLines = data.split(/\r\n|\n/);
-    for (let i = 0; i <= allTextLines.length; i++) {
-        console.log(allTextLines[i])
-    }
 }
 
 function getData(urlData, callback) {
@@ -53,7 +44,7 @@ function getData(urlData, callback) {
         dataType: "text",
         success: function (data) {
 
-            callback(data)
+            callback(data.trim())
         }
     });
 }
