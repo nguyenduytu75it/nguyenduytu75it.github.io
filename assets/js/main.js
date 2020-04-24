a701767f67786d9452bd3fd71a02c363e6f85088
Nguyen Duy Tu <nguyenduytu75it>
2020-04-24 10:09:11 +0700

    aaa

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,28 +7,33 @@ $(document).ready(function () {
 function initialData() {
 
     for (let i = 0; i < urlData.length; i++) {
-        urlSplit = urlData[i].split('/')
-        fileName = urlSplit[urlSplit.length - 1]
-        divContentFile = "<div class='col-md-12 content-file' id='file-" + fileName + "'>"
-        divFileName = "<div class='col-md-12 file-name'><h3>" + fileName + "</h3></div>"
-        
-        data =  getData(urlData)
-        lstCategories = getCategories(data)
-        for (let y = 0; y < lstCategories.length; y++) {
-            divCategory = "<div class='col-md-12 category'><div class='category-title' id='category-title-1'>"+lstCategories[y]+"</div></div>" 
-        }
+        getData(urlData[i], function (data) {
 
-        $(".content-main").append(divContentFile + divFileName + divCategory + "</div>");
+            urlSplit = urlData[i].split('/')
+            fileName = urlSplit[urlSplit.length - 1]
+            divContentFile = "<div class='col-md-12 content-file' id='file-" + fileName + "'>"
+            divFileName = "<div class='col-md-12 file-name'><h3>" + fileName + "</h3></div>"
+            lstCategories = getCategories(data)
+
+            for (let y = 0; y <= lstCategories.length; y++) {
+                divCategory = "<div class='col-md-12 category'><div class='category-title' id='category-title-1'>" + lstCategories[y] + "</div></div>"
+            }
+
+            $(".content-main").append(divContentFile + divFileName + divCategory + "</div>");
+        })
+
+
     }
 
 
 }
 
-function getCategories(data){
-    arrCategories = []
+function getCategories(data) {
+    let = arrCategories = []
     var allTextLines = data.split(/\r\n|\n/);
-    for (let i = 1; i <= allTextLines.length; i++) {
+    for (let i = 0; i <= allTextLines.length; i++) {
         lineSplit = allTextLines[i].split(',')
+        console.log(lineSplit[2])
         arrCategories.push(lineSplit[2])
     }
     return arrCategories
@@ -41,13 +46,14 @@ function processData(data) {
     }
 }
 
-function getData(urlData) {
+function getData(urlData, callback) {
     $.ajax({
         type: "GET",
         url: urlData,
         dataType: "text",
         success: function (data) {
-            return data
+
+            callback(data)
         }
     });
 }
