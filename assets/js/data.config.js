00899d7b64bad419e6fa0c9908d050d66d955b04
Nguyen Duy Tu <nguyenduytu75it>
2020-04-24 09:54:20 +0700

    update

--- assets/js/data.config.js
+++ assets/js/data.config.js
@@ -1,1 +1,5 @@
-url = ["https://raw.githubusercontent.com/nguyenduytu75it/nguyenduytu75it.github.io/master/data/data01.csv"]
+const urlData = [
+    "https://raw.githubusercontent.com/nguyenduytu75it/nguyenduytu75it.github.io/master/data/data01.csv",
+    "https://raw.githubusercontent.com/nguyenduytu75it/nguyenduytu75it.github.io/master/data/data02.csv",
+    "https://raw.githubusercontent.com/nguyenduytu75it/nguyenduytu75it.github.io/master/data/data03.csv"
+]
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,53 @@
-$( document ).ready(function() {
-    $(".category-title").click(function(){
-        console.log($(this).attr("id"))
-        getData()
+$(document).ready(function () {
+    $("#initial-btn").click(function () {
+        initialData()
     })
 });
 
-function getData(){
-    $.ajax({
-        type: "GET",
-        url: "https://raw.githubusercontent.com/nguyenduytu75it/nguyenduytu75it.github.io/master/data/data01.csv",
-        dataType: "text",
-        success: function(data) {
-            processData(data);
+function initialData() {
+
+    for (let i = 0; i < urlData.length; i++) {
+        urlSplit = urlData[i].split('/')
+        fileName = urlSplit[urlSplit.length - 1]
+        divContentFile = "<div class='col-md-12 content-file' id='file-" + fileName + "'>"
+        divFileName = "<div class='col-md-12 file-name'><h3>" + fileName + "</h3></div>"
+        
+        data =  getData(urlData)
+        lstCategories = getCategories(data)
+        for (let y = 0; y < lstCategories.length; y++) {
+            divCategory = "<div class='col-md-12 category'><div class='category-title' id='category-title-1'>"+lstCategories[y]+"</div></div>" 
         }
-     });
+
+        $(".content-main").append(divContentFile + divFileName + divCategory + "</div>");
+    }
+
+
+}
+
+function getCategories(data){
+    arrCategories = []
+    var allTextLines = data.split(/\r\n|\n/);
+    for (let i = 1; i <= allTextLines.length; i++) {
+        lineSplit = allTextLines[i].split(',')
+        arrCategories.push(lineSplit[2])
+    }
+    return arrCategories
 }
 
 function processData(data) {
     var allTextLines = data.split(/\r\n|\n/);
-    for(let i = 0; i <= allTextLines.length; i++){
+    for (let i = 0; i <= allTextLines.length; i++) {
         console.log(allTextLines[i])
     }
+}
+
+function getData(urlData) {
+    $.ajax({
+        type: "GET",
+        url: urlData,
+        dataType: "text",
+        success: function (data) {
+            return data
+        }
+    });
 }
