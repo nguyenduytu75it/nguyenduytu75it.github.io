b9943eb9c3d855422fa0542e4ad850545d150344
Nguyen Duy Tu <nguyenduytu75it>
2020-04-27 10:32:59 +0700

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,67 @@
 $(document).ready(function () {
-        initialData()
+    initialData()
 });
+
+var objData = new Object()
 
 function initialData() {
 
     for (let i = 0; i < urlData.length; i++) {
         getData(urlData[i], function (data) {
+            let urlSplit = urlData[i].split('/')
+            let fileName = urlSplit[urlSplit.length - 1]
+            let object = progressData(data)
 
-            urlSplit = urlData[i].split('/')
-            fileName = urlSplit[urlSplit.length - 1]
-            divContentFile = "<div class='col-md-12 content-file' id='file-" + fileName + "'>"
-            divFileName = "<div class='col-md-12 file-name'><h3>" + fileName + "</h3></div>"
-            lstCategories = getCategories(data)
-            divCategory = ""
+            // divContentFile = "<div class='col-md-12 content-file' id='file-" + fileName + "'>"
+            // divFileName = "<div class='col-md-12 file-name'><h3>" + fileName + "</h3></div>"
+            // lstCategories = getCategories(data)
+            // divCategory = ""
 
-            lstCategories.forEach(category => {
-                divCategory += "<div class='col-md-12 category'><div class='category-title' id='category-title-1'>" + category + "</div></div>"
-            });
+            // lstCategories.forEach(category => {
+            //     divCategory += "<div class='col-md-12 category' id = '" + category
+            //         + "' onclick='generateTable(this)'><div class='category-title' id='category-title-1'>" + category + "</div></div>"
+            // });
 
-            $(".content-main").append(divContentFile + divFileName + divCategory + "</div>");
+            // $(".content-main").append(divContentFile + divFileName + divCategory + "</div>");
+
+            objData[fileName] = object
         })
-
-
     }
 
+    setTimeout(function () {
+        console.log(objData['data01.csv'])
+    }, 2000)
+
+}
+
+
+function progressData(data) {
+    var allTextLines = data.split(/\r\n|\n/);
+    let = arrCategories = []
+    let objCategory = {}
+    let arrHeader = []
+
+    arrHeader = allTextLines[0].split(',')
+
+    for (let i = 1; i < allTextLines.length; i++) {
+        lineSplit = allTextLines[i].split(',')
+        let objLine = {}
+
+        for (let y = 0; y < arrHeader.length; y++) {
+            objLine[arrHeader[y]] = lineSplit[y].replace(/"/g, "")
+        }
+
+        category = lineSplit[2].replace(/"/g, "")
+
+        if (category in objCategory) {
+            objCategory[category].push(objLine)
+        }
+        else {
+            objCategory[category] = [objLine]
+        }
+    }
+
+    return objCategory
 
 }
 
@@ -31,10 +69,11 @@ function getCategories(data) {
     let = arrCategories = []
     var allTextLines = data.split(/\r\n|\n/);
     for (let i = 1; i <= allTextLines.length - 1; i++) {
+
         lineSplit = allTextLines[i].split(',')
-        arrCategories.push(lineSplit[2].replace('"',""))
+        arrCategories.push(lineSplit[2].replace(/"/g, ""))
     }
-    return arrCategories
+    return removeDulicate(arrCategories)
 }
 
 function getData(urlData, callback) {
@@ -43,8 +82,21 @@ function getData(urlData, callback) {
         url: urlData,
         dataType: "text",
         success: function (data) {
-
             callback(data.trim())
         }
     });
+}
+
+function removeDulicate(arrData) {
+    var uniqueNames = [];
+    $.each(arrData, function (i, item) {
+        if ($.inArray(item, uniqueNames) === -1) uniqueNames.push(item);
+    });
+    return uniqueNames
+}
+
+function generateTable(event) {
+
+    console.log($(event).attr("id"))
+    $(event).append("<div class='col-md-12 tbl-data'><h3>abc</h3></div>")
 }
